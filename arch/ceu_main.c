7369a7d56708511b9ea43d264219e0a9bee78354
fsantanna <francisco.santanna@gmail.com>
2016-05-03 08:38:35 -0300

    (+) synchronous FS close

--- arch/ceu_main.c
+++ arch/ceu_main.c
@@ -57,6 +57,8 @@ printf(">>> FREE\n");
 #define ceu_uv_fs_read(a,b,c,d,e)  uv_fs_read(&ceu_uv_loop,a,b,c,d,e,ceu_uv_fs_cb)
 #define ceu_uv_fs_write(a,b,c,d,e) uv_fs_write(&ceu_uv_loop,a,b,c,d,e,ceu_uv_fs_cb)
 #define ceu_uv_fs_close(a,b)       uv_fs_close(&ceu_uv_loop,a,b,ceu_uv_fs_close_cb)
+#define ceu_uv_fs_close_(a,b)      uv_fs_close(&ceu_uv_loop,a,b,NULL); \
+                                   ceu_uv_fs_close_cb(a)
 
 void ceu_uv_fs_cb (uv_fs_t* req) {
 #ifdef CEU_IN_FS
